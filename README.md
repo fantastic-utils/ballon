ecd950ff439beca3dd6a4be5c2c7637b9e281d0d
lucky zhou <zsney.lzhou@gmail.com>
2023-02-21 11:38:20 +0800

    Initial Commit

---
+++ README.md
@@ -0,0 +1,247 @@
+# @fantastic-utils/ballon
+
+A react state management and operation library, it's based on `redux`, `mutative` and `memoize-one`, the library is designed to have a good development experience and a good performance. The API is similar to `vuex` which can help vue developer to quickly work on react project.
+
+- `redux`: is a famous statement management tool.
+- `mutative`: is a `immer` like alternative library which get better performance.
+- `memoize-one`: is a function cache tool, which used to improve performance.
+
+## Installation
+
+```bash
+npm install @fantastic-utils/ballon
+
+```
+
+## Usage
+
+Here is an basic usage case.
+
+```js
+// store.js
+import { createStore } from 'by-state';
+
+const store = createStore({
+  state: { name: 'lucky', age: 12 },
+  actions: {
+    updateAge(state, { age }) {
+      state.age = 20;
+    },
+  },
+  getters: {
+    isAdult(state) {
+      return state.age > 18;
+    },
+    isLuckyAdult(state, getters) {
+      return getters.isAdult && state.name === 'lucky';
+    },
+  },
+});
+
+export default store;
+```
+
+```jsx
+// App.jsx
+import React, { useCallback } from 'react';
+import { useStore } from 'by-state';
+import { ByButton } from 'bitd';
+
+const App = () => {
+  const [state, dispatch, getters, refState] = useStore();
+  const { name, age } = state;
+  const { isLuckyAdult } = getters,
+
+  const handleUpdateAge = useCallback(() => {
+    dispatch({ type: 'updateAge', age: 20 });
+  }, [dispatch]);
+
+  useEffect(() => {
+    const someTimer = setInterval(() => {
+      dispatch({ type: 'doSomeIntervalUpdate', age: refState.current.age });
+    }, 5000);
+    return () => {
+      clearInterval(someTimer);
+    };
+  }, []);
+
+  return (
+    <div>
+      <h1>Name: { name }</h1>
+      <p>Age: { age }</p>
+      <p>{ isLuckyAdult? 'You are adult' : '' }</p>
+      <ByButton onClick={handleUpdateAge}>Update Age</ByButton>
+    </div>
+  )
+};
+
+```
+
+```js
+// index.js
+import ReactDOM from 'react-dom';
+import { StateProvider } from 'by-state';
+import App from './App';
+import store from './store';
+
+ReactDOM.render(
+  <StateProvider store={store}>
+    <App />
+  </StateProvider>,
+  document.getElementById('root')
+);
+```
+
+---
+
+### Api
+
+`createStore(definitionObject: { state: object, getters: object, actions: object, modules: array }, middleware: array)`:
+
+- definitionObject:
+  - `state`: Is the base property object.
+  - `getters`: Is the computed state which will be cached in 1 rendering for decreasing performance loss.
+  - `actions`: Is the way to update state.
+  - `modules`: A list of modules which follow properties.
+    - `ns`: The module namespace.
+    - `state`: The same as above.
+    - `getters`: The same as above.
+    - `actions`: The same as above.
+    - `namespaced`: Is this module should be grouped into a namespaced field.
+      - `state`: State will always grouped into a namespaced field, no matter what value `namespaced` is.
+      - `getters`: Is controlled by `namespaced`, or you can access getters from root getters.
+      - `actions`: Is controlled by `namespaced`, If truthy you should dispatch action with `ns` prefixed, like `dispatch({ type: 'user/updateAge', age: 20 })`, If falsy you can dispatch action directly like `dispatch({ type: 'updateAge', age: 20 })`;
+- middleware: A list of middleware, which relay on `redux`, it's placed between `dispatch` and `action`;
+
+**Returns**: (`ByStateStore` extends `Store` from `redux`)
+
+- `getState()`: get current state.
+- `subscribe(cb)`: subscribe store changes.
+  - cb: the callback function when change happened.
+- `dispatch(action)`: dispatch a action to trigger changes.
+  - action: `{ type: 'xxx', ...other }`
+- `registerModule(module, config)`: to register a module programmatically and dynamically.
+  - moudle: the module response object
+  - config: to config module behaviors.
+
+---
+
+`StateProvider`: The root component to provide store instance.
+
+- `store`: to pass a store instance in.
+
+---
+
+`useStore()`
+
+**Returns**: `[state, dispatch, getters, refState]`
+
+- `state`: The store state
+- `dispatch`: The dispatcher
+- `getters`: the computed states like vuex.
+- `refState`: A reference to state object which won't trigger view update, it's useful to use in hooks which has no dependency but need new state, and it's useful to improve some performance, but be careful with it, is you don't know what you are using.
+
+---
+
+## Advance Usage
+
+### Module
+
+Use Modular store example:
+
+```js
+// store/user.js
+const ns = 'user';
+const namespaced = true;
+
+const state = {
+  name: 'lucky',
+  age: 12,
+};
+
+const getters = {
+  isAdult(state) {
+    return state.user.age > 18;
+  },
+  isLuckyAdult(state, getters) {
+    return getters.user.isAdult && state.user.name === 'lucky';
+  },
+},
+
+const actions = {
+  updateAge(state, { age }) {
+    state.age = 20;
+  },
+};
+
+export {
+  ns,
+  namespaced,
+  state,
+  getters,
+  actions,
+};
+```
+
+```js
+// store/index.js
+import userStore from './user';
+
+const store = createStore({
+  state: { app: 'Demo' },
+  actions: {},
+  getters: {},
+  modules: [userStore],
+});
+
+export default store;
+```
+
+### Dynamic module
+
+Here is an example for dynamic importing an component and register its related module
+
+```jsx
+import loadable from '@loadable/component';
+import { Switch, Route } from 'react-router-dom';
+import store from './store';
+
+const Home = loadable(() => import('./container/About'));
+
+const About = loadable(() =>
+  Promise.all([import('./container/About'), import('./store/about')]).then(
+    ([Component, storeModule]) => {
+      store.registerModule(storeModule);
+      return Component;
+    }
+  )
+);
+
+function App() {
+  return (
+    <Switch>
+      <Route path="/about">
+        <About />
+      </Route>
+      <Route path="/">
+        <Home />
+      </Route>
+    </Switch>
+  );
+}
+```
+
+## Install
+
+```bash
+$ npm install
+```
+
+```bash
+$ npm run dev
+$ npm run build
+```
+
+## LICENSE
+
+MIT
---
+++ src/index.tsx
@@ -0,0 +1,339 @@
+import * as React from 'react';
+import { createStore as createReduxStore, applyMiddleware, Store } from 'redux';
+import { create as produce } from 'mutative';
+import memoizeOne from 'memoize-one';
+import useIsomorphicLayoutEffect from './utils/useIsomorphicLayoutEffect';
+
+const { useContext, useState, useRef } = React;
+
+export enum INTERNAL_TYPES {
+  INIT_MODULE = '$$INIT_MODULE',
+}
+
+export interface RevocableProxy<T> {
+  revoke(): void;
+  proxy: Object;
+};
+
+export type ByStoreAction = {
+  type: string;
+  [key: string]: any;
+};
+export type ByStoreActionFn = (state: Object, action: ByStoreAction) => void;
+export type ByStoreGetterFn = (state: Object, getters: Object) => any;
+
+export interface ByStoreActions {
+  [key: string]: ByStoreActionFn;
+}
+export interface ByStoreGetters {
+  [key: string]: ByStoreGetterFn;
+}
+
+export interface ByStoreModuleBase {
+  default?: ByStoreModule;
+  state: Object;
+  actions?: ByStoreActions;
+  getters?: ByStoreGetters;
+}
+
+export interface ByStoreModule extends ByStoreModuleBase {
+  ns: string;
+  namespaced?: boolean;
+}
+
+export interface ByStoreOptions extends ByStoreModuleBase {
+  state: Object;
+  actions: ByStoreActions;
+  getters: ByStoreGetters;
+  modules: Array<ByStoreModule>;
+}
+
+export interface NormalizedActions {
+  [ns: string]: ByStoreActions;
+}
+export interface NormalizedGetters {
+  [ns: string]: ByStoreGetters;
+}
+export interface NormalizedState {
+  [ns: string]: Object;
+}
+
+export interface NormalizedModule {
+  namespacedNs: Array<string>;
+  actions: NormalizedActions
+  getters: NormalizedGetters
+  state: NormalizedState
+}
+
+export interface ByStateStore extends Store {
+  getProxyGetters: () => RevocableProxy<any>;
+  destroyGetters: () => void;
+}
+
+export interface IStateProvider {
+  store: ByStateStore;
+  children: React.ReactNode;
+}
+
+export interface IDynamicModuleConfig {
+  isLoaded?: boolean;
+}
+
+export interface DynamicModules {
+  config: {
+    [ns: string]: IDynamicModuleConfig;
+  },
+  actions: {
+    [ns: string]: ByStoreActions;
+  },
+  getters: {
+    [ns: string]: ByStoreGetters;
+  },
+  state: {
+    [ns: string]: any;
+  },
+}
+
+export type ContextValue = [any?, any?, any?, any?];
+
+const dynamicModules: DynamicModules = {
+  config: {},
+  actions: {},
+  getters: {},
+  state: {},
+};
+
+const MyContext = React.createContext<ContextValue>([]);
+
+export const StateProvider = ({ store, children }: IStateProvider) => {
+  const { getState, dispatch } = store;
+  const storeState = getState();
+  const [state, setState] = useState(storeState);
+  const refState = useRef(storeState);
+
+  useIsomorphicLayoutEffect(() => {
+    const unsubscribe = store.subscribe(() => {
+      const newState = store.getState();
+      setState(newState);
+      refState.current = newState;
+    });
+
+    return () => {
+      store.destroyGetters();
+      unsubscribe();
+    };
+  }, [store]);
+
+  return (
+    <MyContext.Provider
+      value={[state, dispatch, store.getProxyGetters(), refState]}
+    >
+      {children}
+    </MyContext.Provider>
+  );
+};
+
+export const useStore = () => useContext(MyContext);
+
+const normalizeNamespace = (pattern = '') => {
+  const [ns, target] = pattern.split('/');
+  if (target) return { ns, target };
+  return { target: ns };
+};
+
+const getMemoGetters = (getters: ByStoreGetters = {}) => {
+  const properties = Object.getOwnPropertyNames(getters);
+  return properties.reduce(
+    (prev, property) => ({
+      ...prev,
+      [property]: memoizeOne(getters[property]),
+    }),
+    {}
+  );
+};
+
+const normalizeModules = (modules: Array<ByStoreModule> = []) => {
+  const normalizedModules = modules.reduce((prev, cur) => {
+    const { ns, namespaced, state, actions, getters } = cur;
+
+    if (namespaced && !ns) {
+      console.warn(`Namespaced module doesn't specify the 'ns' property, so this module will be discard.`);
+      return prev;
+    }
+
+    const memoGetters = getMemoGetters(getters);
+    const normalizedActions = namespaced ? { [ns as string]: actions } : actions;
+    const normalizedGetters = namespaced ? { [ns as string]: memoGetters } : memoGetters;
+
+    return {
+      ...prev,
+      namespacedNs: []
+        .concat(prev.namespacedNs, (namespaced ? [ns] : []) as any)
+        .filter(Boolean),
+      state: { ...prev.state, [ns as string]: state },
+      actions: { ...prev.actions, ...normalizedActions },
+      getters: { ...prev.getters, ...normalizedGetters },
+    };
+  }, {} as any);
+  return normalizedModules;
+};
+
+const normalizeImportModule = (module:  ByStoreModule) => module.default || module;
+
+const createReducer = (actions: ByStoreActions) =>
+  produce((draft: any, action) => {
+    const { type } = action;
+    if (!type) throw new Error("Can't dispatch a non-type action!");
+
+    if (type === '$$INIT_MODULE') {
+      const { ns, initState } = action;
+      draft[ns] = initState;
+      return draft;
+    }
+
+    const { ns, target } = normalizeNamespace(type);
+    const actionsNs = ns
+      ? (actions?.[ns] || dynamicModules.actions[ns])
+      : actions;
+
+    return (actionsNs as ByStoreActions)?.[target]?.(draft, action);
+  });
+
+export type proxyGetterMap = {
+  [key: string]: RevocableProxy<ByStoreGetterFn>
+}
+
+let moduleProxyGettersMap: proxyGetterMap = {};
+const clearModuleProxyGettersMap = () => {
+  Object.keys(moduleProxyGettersMap).forEach((key) => {
+    moduleProxyGettersMap[key].revoke();
+  });
+  moduleProxyGettersMap = {};
+};
+
+const gettersProxyHandler = (
+  store: Store,
+  rawGetters: NormalizedGetters | ByStoreGetters = {},
+  namespacedNs: Array<string> = [],
+  rootProxyGetters?: object,
+) => {
+  const properties = Object.getOwnPropertyNames(rawGetters);
+  return {
+    get(target: any, key: string, receiver: any): unknown {
+      if (namespacedNs.indexOf(key) > -1) {
+        const cachedGettersProxy = moduleProxyGettersMap[key];
+        if (cachedGettersProxy) return cachedGettersProxy.proxy;
+        const moduleGettersProxy = Proxy.revocable<NormalizedGetters | ByStoreGetters>(
+          rawGetters[key] as ByStoreGetters,
+          gettersProxyHandler(store, rawGetters[key] as ByStoreGetters, [], receiver)
+        );
+        moduleProxyGettersMap[key] = moduleGettersProxy;
+        return moduleGettersProxy.proxy;
+      }
+      if (properties.indexOf(key) === -1) {
+        return target[key];
+      }
+      const state = store.getState();
+      return (rawGetters?.[key] as ByStoreGetterFn)?.(state, rootProxyGetters || receiver);
+    },
+    set() {
+      return false;
+    },
+  };
+};
+
+const createProxyGetters = (
+  store: Store,
+  rawGetters: NormalizedGetters = {},
+  namespacedNs: Array<string> = []
+) => {
+  const proxyGetters = new Proxy(
+    rawGetters,
+    gettersProxyHandler(store, rawGetters, namespacedNs, undefined)
+  );
+
+  return proxyGetters;
+};
+
+export const createStore = (opts: ByStoreOptions, middleware = []) => {
+  const {
+    modules = [] as Array<ByStoreModule>,
+    state: rootState,
+    actions: rootActions,
+    getters: rootGetters,
+  } = opts;
+  const normalizedModules = normalizeModules(modules);
+
+  const {
+    namespacedNs,
+    actions: moduleActions,
+    state: moduleState,
+    getters: moduleGetters,
+  } = normalizedModules;
+
+  const initState = { ...rootState, ...moduleState };
+  const actions = { ...rootActions, ...moduleActions };
+  const getters = { ...getMemoGetters(rootGetters), ...moduleGetters };
+
+  const store: Store = createReduxStore(
+    createReducer(actions),
+    initState,
+    applyMiddleware(...middleware)
+  );
+
+  let proxyGetters = createProxyGetters(store, getters, namespacedNs);
+
+  const getProxyGetters = () => proxyGetters;
+
+  const updateProxyGetters = () => {
+    proxyGetters = createProxyGetters(store, getters, namespacedNs);
+  };
+
+  const registerModule = (module: ByStoreModule, config: IDynamicModuleConfig = {}) => {
+    const normalizedModule = normalizeImportModule(module);
+    const { ns } = normalizedModule;
+    if (!ns) {
+      // eslint-disable-next-line
+      console.warn('Dynamic Module must specify the `ns` property for namespace.');
+      return;
+    }
+    if (namespacedNs.indexOf(ns) > -1) {
+      // eslint-disable-next-line
+      console.warn(`Module ${ns} exists!`);
+      return;
+    }
+    if (dynamicModules.config[ns] && dynamicModules.config[ns].isLoaded) return;
+
+    dynamicModules.config[ns] = {
+      isLoaded: true,
+      ...config,
+    };
+
+    namespacedNs.push(ns);
+    getters[ns] = normalizedModule.getters || {};
+    dynamicModules.actions[ns] = normalizedModule.actions || {};
+
+    store.dispatch({
+      type: INTERNAL_TYPES.INIT_MODULE,
+      ns,
+      initState: normalizedModule.state,
+    });
+
+    updateProxyGetters();
+  };
+
+  const destroyGetters = () => {
+    proxyGetters = null;
+    clearModuleProxyGettersMap();
+  };
+
+  return {
+    ...store,
+    getProxyGetters,
+    destroyGetters,
+    registerModule,
+    // unRegisterModule,
+  };
+};
+
+export default createStore;
